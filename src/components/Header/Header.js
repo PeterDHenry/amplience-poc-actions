ca75fe3602575e469f0c94013da207cc990037b6
Peter Henry <peter.henry@wiggle.com>
2021-05-04 15:23:47 +0100

    Removing unneeded class and the error it created

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ const Header = () => (
     <div className="grid grid-cols-1 md:grid-cols-12 gap-4">
       <div className="col-span-1 text-center mb-6 md:mb-0">
         <a id="wiggle-logo" href="/" itemProp="url" tabIndex="1">
-          <img class="bem-header__logo" src="//www.wigglestatic.com/images/ui/wiggle-logo/desktop-wiggle_master_rgb_logo.svg" itemProp="logo" />
+          <img src="//www.wigglestatic.com/images/ui/wiggle-logo/desktop-wiggle_master_rgb_logo.svg" itemProp="logo" />
         </a>
       </div>
       <div className="col-span-11 text-center md:text-right">
