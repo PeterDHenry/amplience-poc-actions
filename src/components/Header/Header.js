4a64363c47168618b025367f3e8c10b7a77f52e8
Peter Henry <peter.henry@wiggle.com>
2021-05-04 15:11:32 +0100

    Few bug fixes for non camel cased properties

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,8 +4,8 @@ const Header = () => (
   <div className="bg-gray-800 row-auto p-2.5">
     <div className="grid grid-cols-1 md:grid-cols-12 gap-4">
       <div className="col-span-1 text-center mb-6 md:mb-0">
-        <a id="wiggle-logo" class="bem-header__logo" data-ga-action="Logo" href="/" itemprop="url" tabindex="1">
-          <img class="bem-header__logo" src="//www.wigglestatic.com/images/ui/wiggle-logo/desktop-wiggle_master_rgb_logo.svg" itemprop="logo" />
+        <a id="wiggle-logo" href="/" itemProp="url" tabIndex="1">
+          <img class="bem-header__logo" src="//www.wigglestatic.com/images/ui/wiggle-logo/desktop-wiggle_master_rgb_logo.svg" itemProp="logo" />
         </a>
       </div>
       <div className="col-span-11 text-center md:text-right">
