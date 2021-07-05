319843215ee9175e8fc00477ed2333cf7a815e5e
Kyle Cuthbertson <kyle.cuthbertson@wiggle.com>
2021-07-05 16:51:09 +0100

    PushPanel - Added files and used Styled Components for styling

--- components/LazyLoadImage/PushPanelImage.tsx
+++ components/LazyLoadImage/PushPanelImage.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
-import { SimpleImg } from 'react-simple-img';
 
 function PushPanelImage( props ) {
   const imageUrl = `https://cdn.media.amplience.net/i/wigglecrcnonprd/${props.id}`;
   return (
-    <img src="https://www.wigglestatic.com/images/offers/commercial/2021/Week23/homepagemb2dx.jpg"
+    <img src="https://www.wigglestatic.com/images/offers/commercial/2021/week26b/homepageb3d2.jpg"
     />
   );
 }
 
 PushPanelImage.defaultProps = {
-  id: "homepage-h-d"
+  id: "pushpanel-1"
 }
 
 
