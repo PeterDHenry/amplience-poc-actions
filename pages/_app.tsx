a919b588e52ffb8784f635c8cffec6bfa483f846
PeterDHenry <77453747+PeterDHenry@users.noreply.github.com>
2021-08-02 16:58:51 +0100

    Tidying up remainder of repo and removing unused theme utility

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,9 @@ import { ThemeProvider } from 'styled-components'
 import Header from '../components/Header/Header'
 import Footer from '../components/Footer/Footer'
 import theme from '../themes/light'
+import { AppProps } from 'next/app';
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps }:AppProps) {
   return (
     <>
       <ThemeProvider theme={theme}>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,21 @@
 import Head from 'next/head'
-import Carousel from '../components/Carousel/Carousel'
-import PushPanelAll from '../components/PushPanel/PushPanelRow'
+
 //import { fetchContentById } from '../utils/fetchContent'
 import { fetchContent } from '../utils/fetchContent'
+import { NextPageContext } from 'next'
 
-export const Home = ({ heroBannerList, pushPanelList }): JSX.Element => (
+
+import  { HeroProps } from '../components/Hero/Hero'
+import Carousel from '../components/Carousel/Carousel'
+import PushPanelAll from '../components/PushPanel/PushPanelRow'
+import  { PushPanelProps } from '../components/PushPanel/PushPanel'
+
+export interface IndexProps {
+  heroBannerList: HeroProps[],
+  pushPanelList: PushPanelProps[]
+}
+
+export const Home = ({ heroBannerList, pushPanelList }: IndexProps): JSX.Element => (
   <>
     <Head>
       <title>Wiggle | Cycle | Run | Swim | Tri-Sports &amp; Bike Shop</title>
@@ -15,7 +26,7 @@ export const Home = ({ heroBannerList, pushPanelList }): JSX.Element => (
   </>
 )
 
-const getServerSideProps = async (context) => {
+export const getServerSideProps = async (context: NextPageContext) => {
   const slot = fetchContent('homepage-content-slot', context)
   // const slot = fetchContentById(
   //   '5c433abb-4ec7-4050-a1eb-723ef166d5e8',
