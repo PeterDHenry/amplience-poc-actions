dacab516bcf77302640378ae55a2bf2e802c30f2
PeterDHenry <77453747+PeterDHenry@users.noreply.github.com>
2021-08-10 09:58:16 +0100

    Key placement bugs found by testing

--- components/PushPanel/ThreePushPanel.tsx
+++ components/PushPanel/ThreePushPanel.tsx
@@ -7,8 +7,11 @@ export default function PushPanelRow({ pushPanelList }: PushPanelListProps) {
     <div className="container py-5">
       <div className="row">
         {pushPanelList.map((panel: PushPanelProps) => (
-          <div className="col-xs-6 col-sm-4 w-pr-4 w-pl-2 w-pl-sm-4">
-            <PushPanel key={panel._meta.deliveryId} {...panel} />
+          <div
+            key={panel._meta.deliveryId}
+            className="col-xs-6 col-sm-4 w-pr-4 w-pl-2 w-pl-sm-4"
+          >
+            <PushPanel {...panel} />
           </div>
         ))}
       </div>
