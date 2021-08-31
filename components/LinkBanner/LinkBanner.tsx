eb22bf7f5fc7bc1b0db62f1c71a7e7ee262f5118
Kyle Cuthbertson <KyleKCuthbertson@gmail.com>
2021-08-31 15:17:56 +0100

    Added properties & updated snapshot

--- components/LinkBanner/LinkBanner.tsx
+++ components/LinkBanner/LinkBanner.tsx
@@ -5,8 +5,8 @@ import { LinkBannerProps } from './types'
 const Banner = styled.a<any>`
   padding: 10px 5px;
   margin: 10px 0;
-  background-color: ${(props) => props.background};
-  color: ${(props) => props.colour};
+  background-color: ${(props) => props.backgroundColour};
+  color: ${(props) => props.fontColour};
   text-decoration: none;
   font-family: 'Roboto Condensed';
   display: flex;
@@ -19,11 +19,12 @@ const Banner = styled.a<any>`
     -moz-transition: 0.4s;
     -ms-transition: 0.4s;
     -o-transition: 0.4s;
-    background: ${(props) => props.colour};
+    border: 2px solid #3c3c3b;
+    background: ${(props) => props.fontColour};
     color: #fff;
   }
   &:hover {
-    color: ${(props) => props.colour};
+    color: ${(props) => props.fontColour};
   }
   p {
     margin: 0;
@@ -38,7 +39,7 @@ const BannerText = styled.div<any>`
 `
 
 const BannerCTA = styled.button<any>`
-  border: 2px solid ${(props) => props.colour};
+  border: 2px solid ${(props) => props.fontColour};
   padding: 2px 16px;
   text-align: center;
   background: none;
@@ -52,8 +53,14 @@ const BannerCTA = styled.button<any>`
 
 const LinkBanner = ({
   textAndCTA: { callToActionURL, callToActionText, headline },
+  backgroundColour,
+  fontColour,
 }: LinkBannerProps) => (
-  <Banner href={callToActionURL}>
+  <Banner
+    href={callToActionURL}
+    backgroundColour={backgroundColour}
+    fontColour={fontColour}
+  >
     <BannerText>
       <p>{headline.toUpperCase()}</p>
     </BannerText>
--- components/LinkBanner/types.ts
+++ components/LinkBanner/types.ts
@@ -5,4 +5,6 @@ export interface LinkBannerProps {
     headline: string
     strapline: string
   }
+  backgroundColour: string
+  fontColour: string
 }
